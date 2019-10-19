5f8fee816d2fcf429cb8d45999e9e84de2e9896a
Qing <chingwang.qw@gmail.com>
2019-10-19 17:44:31 +0100

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require_relative './directors_database'
+require "pry"
 
 def directors_totals(source)
   result = {}
@@ -15,29 +16,44 @@ end
 def gross_for_director(d)
   total = 0
   index = 0
-
   while index < d[:movies].length do
     total += d[:movies][index][:worldwide_gross]
     index += 1
   end
-
   total
 end
 
 def list_of_directors(source)
-  # Write this implementation
+  director_list = []
+  director_index = 0
+  while director_index < source.length do
+    director = source[director_index][:name]
+    director_list << director
+  director_index += 1
+  end
+  return director_list
 end
 
 def total_gross(source)
-  # Write this implementation
-  #
+  total = 0
+  director_list = list_of_directors(source)
+  director_total = directors_totals(source)
+  #director_list is a array ["Stephen Spielberg", "Russo Brothers"]
+  #directors_total is a hash {"Stephen Spielberg"=>1357566430, "Russo Brothers"=>2281002470}
+  director_total.each do |key, value|
+    if director_list.include?(key)
+      total += value
+    end
+  end
+  return total
+end
+
+# Write this implementation
   # Should use methods:
   # 1. directors_totals: returns a Hash of { dir_name => gross }
-  # 2. list_of_directors: names provides an Array of directors names (use
-  #
-  # Visit each key (i.e. director name), look up the value in the hash
+  # 2. list_of_directors: names provides an Array of directors names 
+  #(use visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
-end
 
 
